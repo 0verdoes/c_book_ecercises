c46e8ca034883fef092ce3ab6823a8a4846aaabb
Bence Lovász <lovaszbence11@gmail.com>
2024-03-22 15:30:28 +0100

    Hello world program as the first exercise

---
+++ ch01/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//All things have to start somewhere...
+//to build : gcc -W -Wall -Wextra hello.c -o hello.out
+//after succesful build run as: ./hello.out
+
+int main()
+{
+    printf("Hello world!%c", '\n');
+
+    return 0;
+}
