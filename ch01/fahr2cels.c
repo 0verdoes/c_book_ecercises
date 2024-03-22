af6b8a595e944a0aa9c11118960ab9d24aafb6a4
Bence Lovász <lovaszbence11@gmail.com>
2024-03-22 15:57:35 +0100

    Adding backwards iteration and correction strcmp bug

--- ch01/fahr2cels.c
+++ ch01/fahr2cels.c
@@ -7,6 +7,7 @@
    celsius to Fahrenheit according to the comman line arguments
 */
 
+//ecercise 1.4 convert the otherway
 double fahr2cels(double fahr)
 {
     const double fahr2cels_factor = 5./9.;
@@ -25,38 +26,35 @@ double cels2fahr(double cels)
 
 typedef double (*conv_func_t)(double);
 
+enum {CELS_ARGS = 0, BACK_ARGS = 1, ARGS_LAST = 2};
+enum {ARGS_LEN = 5};
 
 int main(int argc, char* argv[])
 {
     //list of accepted arguments
-    static const char* accepted_args[] = {
-        "cels",
-        "celsius",
-        "--celsius",
-        "-C",
-        "-c",
+    static const char* accepted_args[ARGS_LAST][ARGS_LEN] = {
+        {"cels", "celsius",   "--celsius",   "-C", "-c"},
+        {"back", "backwards", "--backwards", "-B", "-b"}
     };
 
     //decoration print
     const char* heading = "FAHR  CELS  \n";
-
     //abraction of conversion logic
     conv_func_t conv = fahr2cels;
+    //1.5: loop backwards exercise
+    bool backwards   = false;
 
     if (1 < argc) {
-        const size_t len = sizeof(accepted_args)/(sizeof(accepted_args[0]));
-        bool not_found = true;
-
-        for(size_t i = 0; i < len && not_found; ++i) {
-            if (strcmp(argv[1], accepted_args[i])) {
-                conv = cels2fahr;
-                heading = "CELS  FAHR  \n";
-                not_found = false;
+        for (int arg = 1; arg < argc; ++arg) {
+            for(size_t i = 0; i < ARGS_LEN; ++i) {
+                if (0 == strcmp(argv[arg], accepted_args[CELS_ARGS][i])) {
+                    conv = cels2fahr;
+                    heading = "CELS  FAHR  \n";
+                }
+                if (0 == strcmp(argv[arg], accepted_args[BACK_ARGS][i])) {
+                    backwards = true;
+                }
             }
-        }
-
-        if (not_found) {
-            printf("unrecognized argument: %64s", argv[1]);
         }
     }
 
@@ -66,11 +64,20 @@ int main(int argc, char* argv[])
 
     //print heading
     printf("%s", heading);
-    for(double temp = lower; temp <= upper; temp += step) {
-        double conv_temp = conv(temp);
-        //printf formatting: 3f-> leave 3 parts for the number
-        //3.5f => display 5 nums after the .
-        printf("%3.0f %7.2f\n", temp, conv_temp);
+    if (!backwards) {
+        for(double temp = lower; temp <= upper; temp += step) {
+            double conv_temp = conv(temp);
+            //printf formatting: 3f-> leave 3 parts for the number
+            //3.5f => display 5 nums after the .
+            printf("%3.0f %7.2f\n", temp, conv_temp);
+        }
+    } else {
+        for(double temp = upper; lower <= temp; temp -= step) {
+            double conv_temp = conv(temp);
+            //printf formatting: 3f-> leave 3 parts for the number
+            //3.5f => display 5 nums after the .
+            printf("%3.0f %7.2f\n", temp, conv_temp);
+        }
     }
 
     return 0;
